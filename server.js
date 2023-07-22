01cf01ce7bff3c969c6ac8cef6237100437d7f45
Desmond Obisi <desmondcutest@yahoo.com>
2023-07-22 08:53:17 +0100

    fix: fix query params for search function

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/chat', async (req, res) => {
   // Got query
   } else if(req.session.state === 'QUERY') {
     query = getQuery(req.body);
-    const places = await searchPlaces(query, req.body); 
+    const places = await searchPlaces(query, location); 
     console.log(places)
     sendMessage(`Here are places near you for ${query}:` + places, req.body);
     req.session.state = 'DONE';
